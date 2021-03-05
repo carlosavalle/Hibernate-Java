1922a8532180d4bda28239dd5c2c151adf7398cb
carlosavalle <carlosavalle@users.noreply.github.com>
2021-03-05 15:11:57 -0300

    initial

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,71 +1,35 @@
 package com.company;
 
-import org.hibernate.HibernateException;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.registry.StandardServiceRegistry;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
-import java.util.Iterator;
-import java.util.List;
+import java.lang.reflect.Executable;
 
 public class Main {
-private static  SessionFactory factory;
+
     public static void main(String[] args) {
-        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
-                .configure()
-                .build();
-        try {
-           factory = new MetadataSources(registry)
-                    .buildMetadata().buildSessionFactory();
-            Session session = factory.openSession();
-            Transaction transaction = session.beginTransaction();
-
-            Student student = new Student();
-            student.setFirstName("Albert");
-            student.setLastName("Mandioni");
-
-            session.save(student);
-
-            transaction.commit();
 
 
-            listStudents();
-            session.close();
-            factory.close();
-
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
-            ex.printStackTrace();
-            StandardServiceRegistryBuilder.destroy(registry);
-        }
-
-    }
-    public static void listStudents(){
-        Session session = factory.openSession();
-        Transaction tx = null;
 
         try {
-            tx = session.beginTransaction();
-            List employees = session.createQuery("FROM Student ").list();
-            for (Iterator iterator = employees.iterator(); iterator.hasNext();){
-                Student student = (Student) iterator.next();
-                System.out.println("");
-                System.out.println("Student_ID: " + student.getId());
-                System.out.println("First Name: " + student.getFirstName());
-                System.out.println("  Last Name: " + student.getLastName());
-                System.out.println();
+            Persistence persistence = new Persistence();
+            // insert a new student into the table
+            persistence.insertStudent("Carlos","Avalle");
+            persistence.insertStudent("Alberto","Mandioni");
+            persistence.insertStudent("Adam","Avalle");
+            persistence.insertStudent("Aaron","Avalle");
+
+
+            //list all students in the db
+            System.out.println("List of Students in the system");
+            for (Student s : persistence.listStudents()){
+                System.out.println(s);
 
             }
-            tx.commit();
-        } catch (HibernateException e) {
-            if (tx!=null) tx.rollback();
-            e.printStackTrace();
-        } finally {
-            session.close();
-        }
-    }
+        }catch (Exception e){
+            System.out.println(e);
 
+        }
+
+
+
+    }
 }
---
+++ src/com/company/Persistence.java
@@ -0,0 +1,84 @@
+package com.company;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+import java.util.List;
+
+public class Persistence {
+    private static SessionFactory factory;
+
+    public Persistence() {
+        // will start the hibernate service with the hibernate file configuration.
+        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure()
+                .build();
+        try {
+            factory = new MetadataSources(registry)
+                    .buildMetadata().buildSessionFactory();
+
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+            ex.printStackTrace();
+            StandardServiceRegistryBuilder.destroy(registry);
+
+        }
+    }
+// will insert a new student to the database and receive fistname and lastname
+    public  void insertStudent(String firstName, String lastName) throws Exception {
+// will validate first name and last name
+      if(Validators.isValidateName(firstName) == true && Validators.isValidateName(lastName) == true) {
+          // open the session
+          Session session = factory.openSession();
+          Transaction transaction = null;
+          try {
+              // begin the transaction
+              transaction = session.beginTransaction();
+            // creates a new student
+              Student student = new Student();
+              // set the fist name and last name
+              student.setFirstName(firstName);
+              student.setLastName(lastName);
+              // save the student and commit the changes.
+              session.save(student);
+              transaction.commit();
+
+          } catch (HibernateException e) {
+              if (transaction != null) transaction.rollback();
+              e.printStackTrace();
+          } finally {
+              session.close();
+          }
+      }else {
+          throw new Exception("Invalid student name");
+      }
+      }
+
+
+    public List<Student> listStudents(){
+        Session session = factory.openSession();
+        Transaction transaction = null;
+        List<Student> students = null;
+        try {
+            transaction = session.beginTransaction();
+            students = session.createQuery("FROM Student ").list();
+            transaction.commit();
+
+        } catch (HibernateException e) {
+            if (transaction!=null) transaction.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+        return students;
+    }
+
+
+
+
+}
